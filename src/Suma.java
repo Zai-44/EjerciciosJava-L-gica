9eec9c0c578d065715bfbc0320a4c2661f5836b6
zaira <zaira00.sand@gmail.com>
2025-06-02 11:56:20 -0600

    ajustes

--- src/Suma.java
+++ src/Suma.java
@@ -1,7 +1,7 @@
 public class Suma {
     void suma(int a, int b) {
         int suma = a + b;
-        System.out.println(suma);
+        System.out.println("El resultado de la operación es: " + suma);
     }
 }
 
